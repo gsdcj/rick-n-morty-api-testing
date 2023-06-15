8392e71969f4ef710d96036814185f0153fd00df
gsdcj <105465400+gsdcj@users.noreply.github.com>
2023-06-15 10:09:32 +0300

    Update main.js
    adding the fetch api calling method

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 /*
 populating the dom with js and fetching the api contents
 */
+fetch('https://rickandmortyapi.com/api/character');
+.then(response => response.json());
+.then(data => show(data.result));
+
+function show(data) {
+   console.log(data);
+ };
